6db0f69dce79570e265358c8f408f91e26a6d0de
RaveHunter05 <paulsotelor@yahoo.es>
2025-10-25 14:24:28 -0600

    feat: controller for reports

---
+++ src/main/java/com/pos/pos/Controller/ReportsController.java
@@ -0,0 +1,12 @@
+package com.pos.pos.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pos.pos.Model.Inventory;
+
+@RestController
+@RequestMapping("api/reports")
+public class ReportsController {
+//@TODO: keep working on this
+}
